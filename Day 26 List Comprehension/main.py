51793ad58ab693ccd261dfdf52b5e2acd8ee04ed
dhaubanjar <rajdhaubanjar007@gmail.com>
2024-11-26 22:58:12 -0600

    In Day 30, Updated code of Day 26, added error handling

--- Day 26 List Comprehension/main.py
+++ Day 26 List Comprehension/main.py
@@ -1,3 +1,4 @@
+""" UPDATE: In Day 30, updated code using error handling."""
 import pandas as pd
 
 """ importing data from csv """
@@ -8,7 +9,14 @@ import pandas as pd
 """ Second option """
 nato = pd.read_csv('nato_phonetic_alphabet.csv')
 phonetic_dict = {row.letter : row.code for (index, row) in nato.iterrows()}     # uses dictionary comprehension
+def generate_phonetic():
+    user_input = input("Enter a word. ").upper()
+    try:
+        compare = {n: phonetic_dict[n] for n in user_input}                             # uses dictionary comprehension
+    except KeyError:
+        print("Enter only letters.\n")
+        generate_phonetic()
+    else:
+        print(compare)
 
-user_input = input("Enter a word. ").upper()
-compare = {n: phonetic_dict[n] for n in user_input}                             # uses dictionary comprehension
-print(compare)
+generate_phonetic()
